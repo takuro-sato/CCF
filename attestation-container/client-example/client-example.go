f4d1df783e41733ac89ee5b4e4604da671a000e9
root <takurosato@microsoft.com--username>
2023-04-06 10:50:35 +0000

    Validate signature of attestation report

--- attestation-container/client-example/client-example.go
+++ attestation-container/client-example/client-example.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"crypto"
+	"crypto/ecdsa"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/binary"
+	"encoding/hex"
 	"encoding/pem"
 	"flag"
+	"fmt"
 	"log"
+	"math/big"
 	"net"
 	"time"
 
@@ -34,6 +40,150 @@ func splitPemChain(pemChain []byte) [][]byte {
 		}
 	}
 	return chain
+}
+
+// Attestation report is based on SEV-SNP ABI Spec
+// https://www.amd.com/system/files/TechDocs/56860.pdf
+
+const (
+	// Table 21
+	ATTESTATION_REPORT_SIZE = 1184
+	SIGNATURE_SIZE          = 512
+)
+
+const (
+	// Encoding for Signing Algorithms
+	// Table 113
+	SIGNATURE_ALGO_ECDSA_P384_SHA384 = 1
+)
+
+type ECDSASignatureP384SHA384 struct {
+	R *big.Int
+	S *big.Int
+}
+
+// Attestation report
+// Table 21
+type SNPAttestationReport struct {
+	// version no. of this attestation report. Set to 1 for this specification.
+	Version uint32 `json:"version"`
+	// The guest SVN
+	GuestSvn uint32 `json:"guest_svn"`
+	// see table 8 - various settings
+	Policy uint64 `json:"policy"`
+	// as provided at launch    hex string of a 16-byte integer
+	FamilyID string `json:"family_id"`
+	// as provided at launch 	hex string of a 16-byte integer
+	ImageID string `json:"image_id"`
+	// the request VMPL for the attestation report
+	VMPL          uint32 `json:"vmpl"`
+	SignatureAlgo uint32 `json:"signature_algo"`
+	// The install version of the firmware
+	PlatformVersion uint64 `json:"platform_version"`
+	// information about the platform see table 22
+	PlatformInfo uint64 `json:"platform_info"`
+	// 31 bits of reserved, must be zero, bottom bit indicates that the digest of the author key is present in AUTHOR_KEY_DIGEST. Set to the value of GCTX.AuthorKeyEn.
+	AuthorKeyEn uint32 `json:"author_key_en"`
+	// must be zero
+	Reserved1 uint32 `json:"reserved1"`
+	// Guest provided data.	64-byte
+	ReportData string `json:"report_data"`
+	// measurement calculated at launch 48-byte
+	Measurement string `json:"measurement"`
+	// data provided by the hypervisor at launch 32-byte
+	HostData string `json:"host_data"`
+	// SHA-384 digest of the ID public key that signed the ID block provided in SNP_LAUNCH_FINISH 48-byte
+	IDKeyDigest string `json:"id_key_digest"`
+	// SHA-384 digest of the Author public key that certified the ID key, if provided in SNP_LAUNCH_FINISH. Zeros if author_key_en is 1 (sounds backwards to me). 48-byte
+	AuthorKeyDigest string `json:"author_key_digest"`
+	// Report ID of this guest. 32-byte
+	ReportID string `json:"report_id"`
+	// Report ID of this guest's mmigration agent. 32-byte
+	ReportIDMA string `json:"report_id_ma"`
+	// Reported TCB version used to derive the VCEK that signed this report
+	ReportedTCB uint64 `json:"reported_tcb"`
+	// reserved 24-byte
+	Reserved2 string `json:"reserved2"`
+	// Identifier unique to the chip 64-byte
+	ChipID string `json:"chip_id"`
+	// The current commited SVN of the firware (version 2 report feature)
+	CommittedSvn uint64 `json:"committed_svn"`
+	// The current commited version of the firware
+	CommittedVersion uint64 `json:"committed_version"`
+	// The SVN that this guest was launched or migrated at
+	LaunchSvn uint64 `json:"launch_svn"`
+	// reserved 168-byte
+	Reserved3 string `json:"reserved3"`
+	// Signature of this attestation report. See table 23. 512-byte
+	Signature string `json:"signature"`
+}
+
+// Deserialize SEV-SNP attestation report
+// Copied from https://github.com/microsoft/confidential-sidecar-containers/blob/d933d0f/pkg/attest/snp.go
+func (r *SNPAttestationReport) DeserializeReport(report []uint8) error {
+
+	if len(report) != ATTESTATION_REPORT_SIZE {
+		return fmt.Errorf("invalid snp report size")
+	}
+
+	r.Version = binary.LittleEndian.Uint32(report[0:4])
+	r.GuestSvn = binary.LittleEndian.Uint32(report[4:8])
+	r.Policy = binary.LittleEndian.Uint64(report[8:16])
+	r.FamilyID = hex.EncodeToString(report[16:32])
+	r.ImageID = hex.EncodeToString(report[32:48])
+	r.VMPL = binary.LittleEndian.Uint32(report[48:52])
+	r.SignatureAlgo = binary.LittleEndian.Uint32(report[52:56])
+	r.PlatformVersion = binary.LittleEndian.Uint64(report[56:64])
+	r.PlatformInfo = binary.LittleEndian.Uint64(report[64:72])
+	r.AuthorKeyEn = binary.LittleEndian.Uint32(report[72:76])
+	r.Reserved1 = binary.LittleEndian.Uint32(report[76:80])
+	r.ReportData = hex.EncodeToString(report[80:144])
+	r.Measurement = hex.EncodeToString(report[144:192])
+	r.HostData = hex.EncodeToString(report[192:224])
+	r.IDKeyDigest = hex.EncodeToString(report[224:272])
+	r.AuthorKeyDigest = hex.EncodeToString(report[272:320])
+	r.ReportID = hex.EncodeToString(report[320:352])
+	r.ReportIDMA = hex.EncodeToString(report[352:384])
+	r.ReportedTCB = binary.LittleEndian.Uint64(report[384:392])
+	r.Reserved2 = hex.EncodeToString(report[392:416])
+	r.ChipID = hex.EncodeToString(report[416:480])
+	r.CommittedSvn = binary.LittleEndian.Uint64(report[480:488])
+	r.CommittedVersion = binary.LittleEndian.Uint64(report[488:496])
+	r.LaunchSvn = binary.LittleEndian.Uint64(report[496:504])
+	r.Reserved3 = hex.EncodeToString(report[504:672])
+	r.Signature = hex.EncodeToString(report[672:1184])
+
+	return nil
+}
+
+func reverseBytes(bytes []byte) []byte {
+	// Deep copy to avoid side effects on the parameter
+	out := append(make([]byte, 0, len(bytes)), bytes...)
+	for i := 0; i < len(out)/2; i++ {
+		j := len(out) - i - 1
+		out[i], out[j] = out[j], out[i]
+	}
+	return out
+}
+
+// Deserialize ECDSA signature with P384 and SHA384
+func deserializeSignature(hexSignature string) (ECDSASignatureP384SHA384, error) {
+	sigBytes, err := hex.DecodeString(hexSignature)
+	if err != nil {
+		log.Fatalf("Failed to decode signature: %s", err.Error())
+	}
+	if len(sigBytes) != SIGNATURE_SIZE {
+		log.Fatalf("Signature length should be %d bytes, but it's %d", SIGNATURE_SIZE, len(sigBytes))
+	}
+	rbytesLittleEndian := sigBytes[0:72]
+	sbytesLittleEndian := sigBytes[72 : 72+72]
+	// Big endian is requred by `SetBytes()`.
+	rbytesBigEndian := reverseBytes(rbytesLittleEndian)
+	sbytesBigEndian := reverseBytes(sbytesLittleEndian)
+	return ECDSASignatureP384SHA384{
+		R: new(big.Int).SetBytes(rbytesBigEndian),
+		S: new(big.Int).SetBytes(sbytesBigEndian),
+	}, nil
 }
 
 func main() {
@@ -140,6 +290,32 @@ QPHfbkH0CyPfhl1jWhJFZasCAwEAAQ==
 	opts.Roots.AddCert(sevVersionCertificate)
 	if _, err := chipCertificate.Verify(opts); err != nil {
 		log.Fatalf("SEV-SNP: The chain of signatures from the root of trust to this attestation is broken" + err.Error())
+	}
+
+	deserializedReport := new(SNPAttestationReport)
+	if err := deserializedReport.DeserializeReport(attestation); err != nil {
+		log.Fatalf("Failed to deserialize attestation report: %s", err.Error())
+	}
+
+	if deserializedReport.SignatureAlgo != SIGNATURE_ALGO_ECDSA_P384_SHA384 {
+		log.Fatalf("Unsupported signature algorithm")
+	}
+
+	sig, err := deserializeSignature(deserializedReport.Signature)
+	if err != nil {
+		log.Fatalf("Failed to deserialize signature: %s", err.Error())
+	}
+
+	signedContents := attestation[:ATTESTATION_REPORT_SIZE-SIGNATURE_SIZE]
+	h := crypto.SHA384.New()
+	h.Write(signedContents)
+	hashedContents := h.Sum(nil)
+
+	valid := ecdsa.Verify(chipCertificate.PublicKey.(*ecdsa.PublicKey), hashedContents, sig.R, sig.S)
+	if valid {
+		log.Println("Attestation report's signature is valid")
+	} else {
+		log.Fatalf("Attestation report's signature is not valid")
 	}
 
 	if len(r.GetUvmEndorsements()) == 0 {
